ca6df14d4862da26de5cc6acd8855f9a3cfeb1f4
stalkeronag <ovovouovryms@gmail.com>
2022-02-25 09:27:37 +0300

    laba3task2

--- laba3Task2.m
+++ laba3Task2.m
@@ -1,23 +1,17 @@
-A=[2.34,-4.21,-11.61;8.04,5.22,0.27;3.92,-7.99,8.37];
-b=[14.41;-6.44;55.36];
-F=b;
-C=A;
-for i=1:size(A,1)
-    F(i)=b(i)/A(i,i);
-    C(i,i)=0;
-    C(i,:)=-C(i,:)/A(i,i);
+A = [2.34,-4.21,-11.61;8.04,5.22,0.27;3.92,-7.99,8.37];
+B = [14.41;-6.44;55.56];
+AB = [A B]; 
+AB = AB([2 3 1],:) 
+for i = 1:size(A) 
+    a(i,1:3) = -AB(i,1:3)/AB(i,i);
+    a(i,i) = 0;
+    b(i,1) = AB(i,4)/AB(i,i);
 end
-currentSet=b;
-N=228;
-k=0;
-while k<N
-    next=C*currentSet+F;
-    if (abs(next-currentSet)<10^-4)
-        break;
-    end
-    currentSet=next;
-    k=k+1;  
-end
-k
-next
-A\b
+eps = 1e-4;
+X0 = b; 
+X1 = a*X0+b;
+while norm(X1-X0)>eps
+    X0 = X1;
+    X1 = a*X0+b;
+end;
+X = X1
